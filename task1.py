d31bc0d6f7f0c170b2220ebb116c4f6a12c33fe5
prvladko <prvladko@gmail.com>
2022-08-01 19:24:57 +0300

    test1 done

--- task1.py
+++ task1.py
@@ -1,4 +1,31 @@
-# There is string s = "Python Bootcamp". Write the code that hashes string.
+import hashlib
+
 s = "Python Bootcamp"
-h = hash(s)
-print(h)
+
+# using hash() method
+hash_value = hash(s)
+print(hash_value)
+
+# using module hashlib with MD5 algorithm
+hash_object = hashlib.md5(s.encode())
+print(hash_object.hexdigest())
+
+# using module hashlib with SHA1 algorithm
+hash_object = hashlib.sha1(s.encode())
+print(hash_object.hexdigest())
+
+# using module hashlib with SHA224 algorithm
+hash_object = hashlib.sha224(s.encode())
+print(hash_object.hexdigest())
+
+# using module hashlib with SHA256 algorithm
+hash_object = hashlib.sha256(s.encode())
+print(hash_object.hexdigest())
+
+# using module hashlib with SHA384 algorithm
+hash_object = hashlib.sha384(s.encode())
+print(hash_object.hexdigest())
+
+# using module hashlib with SHA512 algorithm
+hash_object = hashlib.sha512(s.encode())
+print(hash_object.hexdigest())
